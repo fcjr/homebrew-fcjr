54968cdd7e30b883c8f06b963052da394f4a4ade
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2026-02-17 02:45:52 +0000

    Update local-translate to 0.1.13

--- Casks/local-translate.rb
+++ Casks/local-translate.rb
@@ -1,8 +1,8 @@
 cask "local-translate" do
-  version "0.1.12"
+  version "0.1.13"
 
   url "https://github.com/fcjr/local-translate/releases/download/v#{version}/Local.Translate_#{version}_aarch64.dmg"
-  sha256 "0705b40fc3399618b5fc858c9b30b2f8d90feced3b28b67aa8e9df5b7d6c010e"
+  sha256 "20429ef48461412bc67cb30266b1220893368eae9c29e576048e3799485140dc"
 
   name "Local Translate"
   desc "Privacy-first, offline desktop translation app"
