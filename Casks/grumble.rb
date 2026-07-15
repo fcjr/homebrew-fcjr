08a5a0a453dc76e271c68ee897462d14764fb4ae
Frank Chiarulli Jr. <frank@frankchiarulli.com>
2026-07-15 19:39:32 -0400

    grumble 0.1.8

--- Casks/grumble.rb
+++ Casks/grumble.rb
@@ -1,8 +1,8 @@
 # Template for the Grumble cask. release.yml fills in the version and sha256
 # from the built .dmg and pushes the result to fcjr/homebrew-fcjr.
 cask "grumble" do
-  version "0.1.7"
-  sha256 "23549e8262a481d9ad2872ad93fe46db2bc403cae150a672fdfe2b9a001e76ab"
+  version "0.1.8"
+  sha256 "756f93873f298476c4e3e29bb77d6305f773e17b72b6b270c989a39eb7805d0c"
 
   url "https://github.com/fcjr/grumble/releases/download/v#{version}/Grumble-#{version}.dmg"
   name "Grumble"
